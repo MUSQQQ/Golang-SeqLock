7f263e3cf1855e8cf01d6636b59698b4d5414d60
Jan Walczak <super.walczak@gmail.com>
2021-08-20 13:22:55 +0200

    add resetcounter and timedblocker funcs

--- README.md
+++ README.md
@@ -6,23 +6,20 @@ This project is licensed under the terms of the simplified BSD license.
 ```
 go get -u github.com/MUSQQQ/Golang-SeqLock
 ```
-I'm sorry for the very long example. I plan to prepare a better one inside this repo or make a new repo for showing the usage of this seqlock.
+I'm sorry for not the greatest example. I plan to prepare a better one inside this repo or make a new repo for showing the usage of this seqlock.
 ```go
-var r = rand.New(rand.NewSource(1))
 
 // func that shows the process of reading data
 func ReadingData(seq *seqlock.SeqLock, wg *sync.WaitGroup) {
-	tmp := int32(0)
+	tmp := uint32(0)
 	for {
-		time.Sleep(time.Duration(r.Int31()) * 1)
 		tmp = seq.RdRead()
 		/*
 
-			process of reading data
+			reading data
 
 		*/
 		if !seq.RdAgain(tmp) {
-			fmt.Printf("counter after succesfully reading data: %d\n", tmp)
 			break
 		}
 	}
@@ -31,14 +28,12 @@ func ReadingData(seq *seqlock.SeqLock, wg *sync.WaitGroup) {
 
 // func that shows the process of writing data
 func WritingData(seq *seqlock.SeqLock, wg *sync.WaitGroup) {
-	time.Sleep(time.Duration(r.Int31()) * 1)
 	seq.WrLock()
 	/*
 
-		process of writing data
+		writing data
 
 	*/
-	fmt.Printf("writing in progress, counter is odd: %d\n", seq.Counter)
 	seq.WrUnlock()
 	defer wg.Done()
 }
--- seqlock.go
+++ seqlock.go
@@ -3,38 +3,56 @@ package seqlock
 import (
 	"sync"
 	"sync/atomic"
+	"time"
 )
 
 // Counter increases by 1 everytime the lock gets taken or released by writer
 // sequence number is other name for it
 // inherited mutex is used only by writers
 type SeqLock struct {
-	Counter int32
+	Counter uint32
 	sync.Mutex
 }
 
-// func that reads counter using atomic operations
+// reads counter using atomic operations
 // used before accessing data protected by the lock
-func (seq *SeqLock) RdRead() int32 {
-	return atomic.LoadInt32(&seq.Counter)
+func (seq *SeqLock) RdRead() uint32 {
+	return atomic.LoadUint32(&seq.Counter)
 }
 
-// func that checks if data is not being modified by writer
+// checks if data is not being modified by writer
 // or if it was not modified since rdRead func
-func (seq *SeqLock) RdAgain(val int32) bool {
-	return (atomic.LoadInt32(&seq.Counter)&1) != 0 || val != seq.Counter
+func (seq *SeqLock) RdAgain(val uint32) bool {
+	return (atomic.LoadUint32(&seq.Counter)&1) != 0 || val != seq.Counter
 }
 
-// writer lock func, Counter becomes odd when writer
+// resets counter to zero
+func (seq *SeqLock) ResetCounter() {
+	seq.Lock()
+	atomic.SwapUint32(&seq.Counter, 0)
+	seq.Lock()
+}
+
+// counter becomes odd when writer
 // starts modifying data
 func (seq *SeqLock) WrLock() {
 	seq.Lock()
-	atomic.AddInt32(&seq.Counter, 1)
+	atomic.AddUint32(&seq.Counter, 1)
 }
 
-// writer unlock func, Counter becomes even when writer
+// counter becomes even when writer
 // starts modifying data
 func (seq *SeqLock) WrUnlock() {
-	atomic.AddInt32(&seq.Counter, 1)
+	atomic.AddUint32(&seq.Counter, 1)
+	seq.Unlock()
+}
+
+// locks the data for both writers and readers
+// for the given amount of miliseconds
+func (seq *SeqLock) TimedBlocker(ms uint64) {
+	seq.Lock()
+	atomic.AddUint32(&seq.Counter, 1)
+	time.Sleep(time.Duration(ms) * time.Millisecond)
+	atomic.AddUint32(&seq.Counter, 1)
 	seq.Unlock()
 }
